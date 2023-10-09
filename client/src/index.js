f9d1fb0fac2a72c900ee1471415530ff9da7e3a8
Isaac Orraiz <isaacorraiz@gmail.com>
2023-10-09 10:36:23 -0400

    Optimized for development

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import "@fortawesome/fontawesome-free/css/all.css";
+//import "@fortawesome/fontawesome-free/css/all.css";
 import Modal from "./components/Modal";
 import "./css/style.css";
 import IdeaForm from "./components/IdeaForm";
